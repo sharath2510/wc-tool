4b51be0c25323e1a5d24f66790801956ed389124
Sharath Rajesh <sharath.rajesh25@gmail.com>
2024-05-10 10:21:09 +0800

    Count number of bytes in file

--- ccwc.py
+++ ccwc.py
@@ -2,16 +2,20 @@ import argparse
 
 def args_setup():
     parser = argparse.ArgumentParser(description="Count number of bytes/lines/words/characters in file")
-    parser.add_argument("-c", type=str, nargs="?", help="Count number of bytes in file")
+    parser.add_argument("-c", action="store_true", help="Count number of bytes in file")
     return parser.parse_args()
 
 def main(args):
-    count_bytes()
+    if args.c:
+        count_bytes()
 
 def count_bytes():
-    with open("test.txt", "rb") as file:
-        for line in file:
-            print(line)
+    # Open file in binary mode "rb" to count number of bytes
+    file_name = "test.txt"
+    with open(file_name, "rb") as file:
+        file_content = file.read()
+        num_bytes = len(file_content)
+        print(f"{num_bytes} {file_name}")
 
 if __name__ == "__main__":
     args = args_setup()
