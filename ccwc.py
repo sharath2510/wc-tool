ecef1aa2b74caa96bcb722104533e233205f4334
Sharath Rajesh <sharath.rajesh25@gmail.com>
2024-05-10 19:05:03 +0800

    Add locale library for characters

--- ccwc.py
+++ ccwc.py
@@ -1,11 +1,27 @@
 import argparse
 import chardet #pip install chardet
+import locale
+
+def supports_multibyte():
+    current_locale = locale.getlocale()
+    if current_locale:
+        encoding = current_locale[1]
+        print(encoding)
+        if encoding:
+            return "UTF-8" in encoding or "utf-8" in encoding
+    return False
+
+if supports_multibyte():
+    print("Current locale supports multibyte characters")
+else:
+    print("Current locale does not support multibyte characters")
 
 def args_setup():
     parser = argparse.ArgumentParser(description="Count number of bytes/lines/words/characters in file")
     parser.add_argument("-c", action="store_true", help="Count number of bytes in file")
     parser.add_argument("-l", action="store_true", help="Count number of lines in file")
     parser.add_argument("-w", action="store_true", help="Count number of words in file")
+    parser.add_argument("-m", action="store_true", help="Count number of characters in file")
     args = parser.parse_args()
 
     # If none of the options are provided, default to True for all options
@@ -23,6 +39,8 @@ def main(args):
         count_lines(file_name, encoding)
     if args.w:
         count_words(file_name, encoding)
+    if args.m:
+        count_characters(file_name, encoding)
 
 def detect_file_encoding(file_name):
     with open(file_name, 'rb') as file:
@@ -51,6 +69,14 @@ def count_words(file_name, encoding):
         for line in file:
             word_count += len(line.split())
     print(word_count, end=' ')
+
+def count_characters(file_name, encoding):
+    print(locale.getencoding())
+    print(locale.getlocale())
+    with open(file_name, "r", encoding=encoding) as file:
+        content = file.read()
+        character_count = len(content)
+    print(character_count, end=' ')
 
 if __name__ == "__main__":
     args = args_setup()
